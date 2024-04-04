8ebb8c1908130d303116de4e93878b7e1010e234
ANGOTHU RAHUL NAIK <102622655+arahuln@users.noreply.github.com>
2024-04-04 17:34:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,29 +26,29 @@ mongoDBConnect();
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Listening at PORT - ${process.env.PORT}`);
 });
-const io = new Server.Server(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: process.env.BASE_URL,
-  },
-});
-io.on('connection', (socket) => {
-  socket.on('setup', (userData) => {
-    socket.join(userData.id);
-    socket.emit('connected');
-  });
-  socket.on('join room', (room) => {
-    socket.join(room);
-  });
-  socket.on('typing', (room) => socket.in(room).emit('typing'));
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+// const io = new Server.Server(server, {
+//   pingTimeout: 60000,
+//   cors: {
+//     origin: process.env.BASE_URL,
+//   },
+// });
+// io.on('connection', (socket) => {
+//   socket.on('setup', (userData) => {
+//     socket.join(userData.id);
+//     socket.emit('connected');
+//   });
+//   socket.on('join room', (room) => {
+//     socket.join(room);
+//   });
+//   socket.on('typing', (room) => socket.in(room).emit('typing'));
+//   socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
 
-  socket.on('new message', (newMessageRecieve) => {
-    var chat = newMessageRecieve.chatId;
-    if (!chat.users) console.log('chats.users is not defined');
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieve.sender._id) return;
-      socket.in(user._id).emit('message recieved', newMessageRecieve);
-    });
-  });
-});
+//   socket.on('new message', (newMessageRecieve) => {
+//     var chat = newMessageRecieve.chatId;
+//     if (!chat.users) console.log('chats.users is not defined');
+//     chat.users.forEach((user) => {
+//       if (user._id == newMessageRecieve.sender._id) return;
+//       socket.in(user._id).emit('message recieved', newMessageRecieve);
+//     });
+//   });
+// });
