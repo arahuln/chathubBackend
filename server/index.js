ebdb4a29347bb567a9e691921bdaf30c00697503
ANGOTHU RAHUL NAIK <102622655+arahuln@users.noreply.github.com>
2024-04-04 17:23:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server.Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL,
   },
 });
 io.on('connection', (socket) => {
